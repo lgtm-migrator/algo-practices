a5b5d25a5362557e959a1c26d6e79e2d17a6b598
Fumiya Tanaka <fumiya.tennis1@gmail.com>
2021-06-15 00:55:52 +0900

    Update sort.py

--- sort.py
+++ sort.py
@@ -10,3 +10,12 @@ def bubble_sort(list: List[int]) -> List[int]:
         list[i], list[i+1] = list[i+1], list[i]
         change = True
   return list
+
+def select_sort(list: List[int]) -> List[int]:
+  for index in range(len(list)-1):
+    min = index
+    for j in range(index+1, len(list)):
+      if list[j] < list[min]:
+        min = j
+    list[index], list[min] = list[min], list[index]
+  return list
