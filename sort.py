7a0942c33f84e1e4c9ca1f644d20521b3ff2331d
Fumiya Tanaka <fumiya.tennis1@gmail.com>
2021-06-15 00:01:19 +0900

    Add BubbleSort

--- sort.py
+++ sort.py
@@ -1,1 +1,10 @@
 
+def bubble_sort(list: List<int>) -> List<int>:
+  change = True
+  while change:
+    change = False
+    for i in range(len(list)-1):
+      if list[i] > list[i+1]:
+        list[i], list[i+1] = list[i+1], list[i]
+        change = True
+  return list
