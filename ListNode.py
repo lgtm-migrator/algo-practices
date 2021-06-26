5bb3a9bf346d1a87c5866e280fbaad526797149e
Fumiya Tanaka <fumiya.tennis1@gmail.com>
2021-06-26 16:44:43 +0900

    Update ListNode.py

--- ListNode.py
+++ ListNode.py
@@ -1,1 +1,20 @@
+class ListNode:
+  
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
 
+    
+def preorder(root: ListNode) -> List[int]:
+  ret = []
+  stack = [root]
+  
+  while stack:
+    node = stack.pop()
+    if node:
+      ret.append(node.val)
+      stack.append(node.right)
+      stack.append(node.left)
+      
+  return ret
