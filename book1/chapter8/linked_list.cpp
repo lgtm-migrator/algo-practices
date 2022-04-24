ed607d853205ceb869ebdf454c2a8adf5a58a3bd
fummicc1 <fumiya.tennis1@gmail.com>
2022-04-24 23:35:53 +0900

    Update 8.4.1

--- book1/chapter8/linked_list.cpp
+++ book1/chapter8/linked_list.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -10,8 +12,26 @@ struct Node
     Node(string _name = "") : next(NULL), name(_name) {}
 };
 
+Node *nil;
+
+void initialize()
+{
+    nil = new Node();
+    nil->next = nil;
+}
+
+void printList()
+{
+    Node *current = nil->next;
+    for (; current != nil; current = current->next)
+    {
+        cout << current->name << " -> ";
+    }
+    cout << endl;
+}
+
 // ノードpの直後にノードvを挿入する
-void insert(Node *v, Node *p)
+void insert(Node *v, Node *p = nil)
 {
     v->next = p->next;
     p->next = v;
@@ -19,5 +39,23 @@ void insert(Node *v, Node *p)
 
 int main()
 {
+    initialize();
+
+    // 配列
+    vector<string> names = {
+        "yamamoto",
+        "watanabe",
+        "tanaka",
+        "yamada",
+        "aoki",
+    };
+    for (int i = 0; i < names.size(); i++)
+    {
+        Node *node = new Node(names[i]);
+        // 先頭に入れる
+        insert(node);
+        cout << "step " << i << ": ";
+        printList();
+    }
     return 0;
 }
