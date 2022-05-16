1b85aea962696e96f5a79542f5f2166a372ce883
fummicc1 <fumiya.tennis1@gmail.com>
2022-05-16 22:55:42 +0900

    A.py AC

--- previous_atcoders/ARC140/A.py
+++ previous_atcoders/ARC140/A.py
@@ -2,16 +2,23 @@ N, K = map(int, input().split())
 S = input()
 
 divs = []
-for i in reversed(range(1, N+1)):
+i = 1
+while i*i <= N:
     if N % i == 0:
         divs.append(i)
+        divs.append(N//i)
+    i += 1
 
 best = 2 ** 60
-for div in divs:
-    characters = {d: [] for d in div}
+for div in sorted(list(set(divs))):
+    characters = {d: [] for d in range(div)}
     for i, c in enumerate(S):
         characters[i % div].append(c)
-    count = 2 ** 60
-    for index, chars in characters.items() {
-        count += len(set(chars))
-    }
+    count = 0
+    for index, chars in characters.items():
+        m = max(map(lambda c: chars.count(c), set(chars)))
+        count += N//div - m
+    if count <= K:
+        best = min(best, div)
+
+print(best)
