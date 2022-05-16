3ea21956964de788e7785bdfd20bb0d27b0ba29d
fummicc1 <fumiya.tennis1@gmail.com>
2022-05-16 17:58:56 +0900

    wip

--- previous_atcoders/ARC140/A.py
+++ previous_atcoders/ARC140/A.py
@@ -1,29 +1,17 @@
 N, K = map(int, input().split())
-s = input()
-characters = [c for c in s]
+S = input()
 
-dp = [[2 ** 60] * N for j in range(K)]
+divs = []
+for i in reversed(range(1, N+1)):
+    if N % i == 0:
+        divs.append(i)
 
-
-# K回の操作
-for k in range(0, K):
-    kinds = set(characters)
-    # j番目の文字を変える
-    for j in range(0, N):
-        max_eval = 0
-        for kind in kinds:
-            if kind == characters[j]:
-                continue
-            s = characters[:j] + [kind] + characters[j+1:]
-            head = s[:len(s)//2]
-            tail = s[len(s)//2:]
-            eval = 0
-            for h, t in zip(head, tail):
-                if h == t:
-                    eval += 1
-            if eval > max_eval:
-                max_eval = eval
-        dp[k][j] = min(dp[k][j-1], dp[])
-
-
-print(dp[K-1][N-1])
+best = 2 ** 60
+for div in divs:
+    characters = {d: [] for d in div}
+    for i, c in enumerate(S):
+        characters[i % div].append(c)
+    count = 2 ** 60
+    for index, chars in characters.items() {
+        count += len(set(chars))
+    }
