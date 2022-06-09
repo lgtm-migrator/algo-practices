4dbd7f75e741203135f0206cb441f5dbc16cd3cd
fummicc1 <fumiya.tennis1@gmail.com>
2022-06-10 00:27:45 +0900

    Add C.py

--- atcoder/ABC241/C.py
+++ atcoder/ABC241/C.py
@@ -7,50 +7,41 @@ for i in range(N):
 
 can = False
 
-for i in range(N-6+1):
+for i in range(N):
     if can:
         break
-    max_i = i + 5
-    is_last_i = max_i == N - 1
-    if max_i >= N:
-        continue
-    for j in range(N-6+1):
+    for j in range(N):
         if can:
             break
-        max_j = j + 5
-        is_last_j = j == N-1
-        if max_j >= N:
-            continue
-        values = S[j:max_j+1][i:max_i+1]
-        # 縦を探す
-        if is_last_i:
+        # 縦を見る
+        if i + 5 < N:
+            cnt = 0
             for k in range(6):
-                if values[:][k].count("#") >= 4:
-                    can = True
-                    break
-        else:
-            if values[:][0].count("#") >= 4:
+                if S[i+k][j] == "#":
+                    cnt += 1
+            if cnt >= 4:
                 can = True
                 break
-
-        # 横を探す
-        if is_last_j:
+        # 横を見る
+        if j + 5 < N:
+            cnt = 0
             for k in range(6):
-                if values[k][:].count("#") >= 4:
-                    can = True
-                    break
-        else:
-            if values[0][:].count("#") >= 4:
+                if S[i][j+k] == "#":
+                    cnt += 1
+            if cnt >= 4:
                 can = True
                 break
-
-        # 斜を探す
-        nums = []
-        for k in range(6):
-            nums.append(values[k][k])
-        if nums.count("#") >= 4:
-            can = True
-            break
+        if j + 5 < N and i + 5 < N:
+            group = 0
+            rev_group = 0
+            for k in range(6):
+                if S[i+k][j+k] == "#":
+                    group += 1
+                if S[i+k][j + 5 - k] == "#":
+                    rev_group += 1
+            if group >= 4 or rev_group >= 4:
+                can = True
+                break
 
 
 print("Yes" if can else "No")
