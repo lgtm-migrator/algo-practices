9f4193c927271ef6e581cdfdd2d3a925ed870eda
fummicc1 <fumiya.tennis1@gmail.com>
2022-06-17 14:51:34 +0900

    dekita

--- atcoder/TypicalProblem/002.py
+++ atcoder/TypicalProblem/002.py
@@ -1,3 +1,23 @@
-
+N = int(input())
 
 for i in range(2 ** N):
+    s = ""
+    for j in reversed(range(N)):
+        if (i & (1 << j)) == 0:
+            s += "("
+        else:
+            s += ")"
+    open_cnt = 0
+    close_cnt = 0
+    ok = True
+    for c in s:
+        if c == ")":
+            close_cnt += 1
+        else:
+            open_cnt += 1
+        if open_cnt < close_cnt:
+            ok = False
+            break
+    if ok:
+        if open_cnt == close_cnt:
+            print(s)
