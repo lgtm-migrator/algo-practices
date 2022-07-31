836d798e380544b2def7860b2ddca2a9427e977d
fummicc1 <fumiya.tennis1@gmail.com>
2022-08-01 00:51:35 +0900

    Updated bootstrap.sh

--- atcoder/bootstrap.sh
+++ atcoder/bootstrap.sh
@@ -1,7 +1,8 @@
 folder_path=$1
 
 mkdir -p $folder_path
-SHELL_PATH=$(pwd -P)
+dir=$(cd $(dirname $0); pwd)
+SHELL_PATH=$dir
 cd $folder_path
 echo $SHELL_PATH
 ln -s $SHELL_PATH/ac-library/atcoder atcoder
