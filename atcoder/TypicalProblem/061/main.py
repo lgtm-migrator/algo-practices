4f9d9e4f6355333572e7f0e809c70cd7de5145ac
fummicc1 <fumiya.tennis1@gmail.com>
2022-08-11 15:00:43 +0900

    [TypicalProblem] 061

---
+++ atcoder/TypicalProblem/061/main.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from typing import Generic, Iterable, Iterator, TypeVar, Union, List
+import datetime
+import re
+from bisect import bisect_left, bisect_right
+import random
+import heapq
+from queue import PriorityQueue
+from collections import Counter
+import itertools
+from collections import deque
+import math
+import decimal
+import sys
+
+sys.setrecursionlimit(10**6)
+# 文字列を入力する場合は改行が残る
+input = sys.stdin.readline
+# 文字列を入力する場合
+# input_str = input()[:-1]
+
+INF = 2 << 60
+MOD = 998244353
+
+Q = int(input())
+
+que = deque()
+
+for i in range(Q):
+    t, x = map(int, input().split())
+    if t == 1:
+        que.appendleft(x)
+    elif t == 2:
+        que.append(x)
+    elif t == 3:
+        print(que[x-1])
